7747da8e21a779f105714e952ff319023f684bf5
John Bartucz <jbartucz@umn.edu>
2026-06-28 09:09:05 -0500

    ,

--- datenight.py
+++ datenight.py
@@ -622,7 +622,7 @@ def generate_html_file(events, nice_date, filename):
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Date Night - {nice_date}</title>
+    <title>Twin Cities Date Night - {nice_date}</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700&display=swap" rel="stylesheet">
